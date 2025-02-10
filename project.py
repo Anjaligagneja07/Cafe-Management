690a72399dca53fb026e532522de7c1bd473cb65
Anjaligagneja07 <anjaligagneja93@gmail.com>
2025-02-10 19:04:56 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,42 @@
+#cafe project   
+# total={"bill":0}                                                                                        
+# def again():
+#     print("do you want bill or order more?")
+#     print("enter 1 for more order")
+#     print("enter 2 for bill")
+#     c=int(input("enter a number"))
+#     if(c==1):
+#         happy()
+#     if(c==2):
+#         print("Thankyou","anjali","your bill is",total["bill"])
+        
+                
+# def happy():
+#     print("enter 1 for maggie")
+#     print("enter 2 for momos")
+#     print("enter 3 for coffee")
+#     print("enter 4 for panipuri")
+#     print("enter 5 for cake")
+#     a=int(input("enter a number"))
+#     if(a==1):
+#         print("maggie")
+#         total["bill"]+=40
+#         again()
+#     if(a==2):
+#         print("momos")
+#         total["bill"]+=70
+#         again()
+#     if(a==3):
+#         print("coffee")
+#         total["bill"]+=50
+#         again()
+#     if(a==4):
+#         print("panipuri")
+#         total["bill"]+=30
+#         again()
+#     if(a==5):
+#         print("cake")
+#         total["bill"]+=100
+#         again()
+
+# happy()
